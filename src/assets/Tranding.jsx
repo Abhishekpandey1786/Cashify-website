41634007f9a28d7638c96516c20c71aa2c97b6b4
Abhishek Pandey <your_abhishekpandey3119@gmail.com>
2025-05-17 23:45:39 +0530

    Your message describing the changes

--- src/assets/Tranding.jsx
+++ src/assets/Tranding.jsx
@@ -3,12 +3,13 @@ import React from "react";
 const Tranding = () => {
   return (
     <div className="bg-gray-200 pt-5">
-      <h1 className="text-7xl sm:text-3xl font-bold ml-6 sm:ml-24 mt-10 mb-6">
+      {/* Responsive heading size & margin */}
+      <h1 className="text-3xl sm:text-3xl font-bold ml-6 sm:ml-35 mt-5 mb-6">
         Trending Articles
       </h1>
 
       {/* Top 3 featured cards */}
-      <div className="flex flex-wrap justify-center gap-14 px-6 sm:px-24">
+      <div className="flex flex-wrap justify-center gap-8 px-4 sm:px-24">
         {/* Card 1 */}
         <div className="w-full sm:w-96">
           <img
@@ -64,7 +65,7 @@ const Tranding = () => {
       </div>
 
       {/* Grid layout for smaller articles */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-9 px-6 sm:px-26 my-14">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-1 px-4 sm:px-35 my-14">
         {[
           {
             title: "12GB RAM vs 8GB RAM: How Much Do You Really Need in 2025?",
